7ec5e145f249eedf72c92455a5d93769a04f0b9c
EvgeniyKiva <97800267+EvgeniyKiva@users.noreply.github.com>
2022-02-26 15:50:34 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,20 +3,14 @@
 
 int main() {
     std::vector <int> vec {-100,-50, -5, 1, 10, 15};
-    std::vector <int> module (vec.size(),0);
 
-    for(int i=0; i<vec.size(); i++){
-        module[i]= abs(vec[i]);
-    }
     for (int i=0; i<vec.size(); i++){
         for (int j=0; j<vec.size()-i-1; j++){
-            if (module[j]>module[j+1]){
-                int temp = module [j];
-                module[j]=module[j+1];
-                module[j+1]=temp;
-                temp = vec [j];
+            if (abs(vec[j])>abs(vec[j+1])){
+                int temp = vec[j];
                 vec[j]=vec[j+1];
                 vec[j+1]=temp;
+
             }
         }
     }
